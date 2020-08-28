0b533dd7ae7923dedfa6aed96c20d6db8e194cdb
AnmolLimje <softwaredev31@gmail.com>
2020-08-28 09:29:33 +0530

    Added Doughnut chart Support

--- js/mainscript.js
+++ js/mainscript.js
@@ -373,9 +373,13 @@ fetch(HOSTAPI_NMC_AreaWise)
     .then((response) => {
         response.json().then((data) => {
             nagpurdata = data;
+            pieLabels=[];
+            pieDatas=[];
             //console.log(data);
             tableBody = document.querySelector("#areaWiseData");
             for (let index = 0; index < nagpurdata.length; index++) {
+                pieLabels[index]=nagpurdata[index].AREA_NAME;
+                pieDatas[index]=nagpurdata[index].ACTIVE;
                 //console.log(nagpurdata[index]);
                 tableBody.innerHTML += "<tr>"
                     + "<th scope='row'><span class='table - td - title'>" + nagpurdata[index].AREA_NAME + "</th>"
@@ -384,6 +388,129 @@ fetch(HOSTAPI_NMC_AreaWise)
                     + "<td><span class='table - td - title'>" + nagpurdata[index].TOTAL_RECOVERED + "</td>"
                     + "</tr>";
             }
+
+            // For a Doughnut chart
+            Chart.pluginService.register({
+                beforeDraw: function(chart) {
+                  if (chart.config.options.elements.center) {
+                    // Get ctx from string
+                    var ctx = chart.chart.ctx;
+              
+                    // Get options from the center object in options
+                    var centerConfig = chart.config.options.elements.center;
+                    var fontStyle = centerConfig.fontStyle || 'Arial';
+                    var txt = centerConfig.text;
+                    var color = centerConfig.color || '#000';
+                    var maxFontSize = centerConfig.maxFontSize || 75;
+                    var sidePadding = centerConfig.sidePadding || 20;
+                    var sidePaddingCalculated = (sidePadding / 100) * (chart.innerRadius * 2)
+                    // Start with a base font of 30px
+                    ctx.font = "30px " + fontStyle;
+              
+                    // Get the width of the string and also the width of the element minus 10 to give it 5px side padding
+                    var stringWidth = ctx.measureText(txt).width;
+                    var elementWidth = (chart.innerRadius * 2) - sidePaddingCalculated;
+              
+                    // Find out how much the font can grow in width.
+                    var widthRatio = elementWidth / stringWidth;
+                    var newFontSize = Math.floor(30 * widthRatio);
+                    var elementHeight = (chart.innerRadius * 2);
+              
+                    // Pick a new font size so it will not be larger than the height of label.
+                    var fontSizeToUse = Math.min(newFontSize, elementHeight, maxFontSize);
+                    var minFontSize = centerConfig.minFontSize;
+                    var lineHeight = centerConfig.lineHeight || 25;
+                    var wrapText = false;
+              
+                    if (minFontSize === undefined) {
+                      minFontSize = 20;
+                    }
+              
+                    if (minFontSize && fontSizeToUse < minFontSize) {
+                      fontSizeToUse = minFontSize;
+                      wrapText = true;
+                    }
+              
+                    // Set font settings to draw it correctly.
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'middle';
+                    var centerX = ((chart.chartArea.left + chart.chartArea.right) / 2);
+                    var centerY = ((chart.chartArea.top + chart.chartArea.bottom) / 2);
+                    ctx.font = fontSizeToUse + "px " + fontStyle;
+                    ctx.fillStyle = color;
+              
+                    if (!wrapText) {
+                      ctx.fillText(txt, centerX, centerY);
+                      return;
+                    }
+              
+                    var words = txt.split(' ');
+                    var line = '';
+                    var lines = [];
+              
+                    // Break words up into multiple lines if necessary
+                    for (var n = 0; n < words.length; n++) {
+                      var testLine = line + words[n] + ' ';
+                      var metrics = ctx.measureText(testLine);
+                      var testWidth = metrics.width;
+                      if (testWidth > elementWidth && n > 0) {
+                        lines.push(line);
+                        line = words[n] + ' ';
+                      } else {
+                        line = testLine;
+                      }
+                    }
+              
+                    // Move the center up depending on line height and number of lines
+                    centerY -= (lines.length / 2) * lineHeight;
+              
+                    for (var n = 0; n < lines.length; n++) {
+                      ctx.fillText(lines[n], centerX, centerY);
+                      centerY += lineHeight;
+                    }
+                    //Draw text in center
+                    ctx.fillText(line, centerX, centerY);
+                  }
+                }
+              });
+
+            var ctx = document.getElementById('mypiechart').getContext('2d');
+            var myPieChart = new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: pieLabels,
+                    datasets: [{
+                        label: '# of Active Cases',
+                        data: pieDatas,
+                        backgroundColor: [
+                            'rgba(200,200, 200, 0.6)',
+                            'rgba(200, 0, 0, 0.6)',
+                            'rgba(0, 200,0, 0.6)',
+                            'rgba(0, 0, 200, 0.6)',
+                            'rgba(200,200 ,0, 0.6)',
+                            'rgba(0, 200, 200, 0.6)',
+                            'rgba(200, 0, 200, 0.6)',
+                            'rgba(100, 0, 0, 0.5)',
+                            'rgba(0, 100, 0, 0.5)',
+                            'rgba(0, 0, 100, 0.5)',
+                        ],
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    //For doughnut chart
+                    elements: {
+                        center: {
+                          text: 'Active Cases',
+                          color: '#FF6384', // Default is #000000
+                          fontStyle: 'Arial', // Default is Arial
+                          sidePadding: 20, // Default is 20 (as a percentage)
+                          minFontSize: 20, // Default is 20 (in px), set to false and text will not wrap.
+                          lineHeight: 25 // Default is 25 (in px), used for when text wraps
+                        }
+                    }
+                 }
+            });
 
         })
     })
